7388f2313c041504b0e781acd213b4ac506cb63d
khickey25 <kevin.hickey@student.fairfield.edu>
2020-04-02 18:08:23 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Scratch work for medial image interpretability analysis project',
+    author='Kevin',
+    license='MIT',
+)
